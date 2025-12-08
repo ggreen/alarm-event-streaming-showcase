2abc716c6f984a9dbbfa5856697431a895863183
ggreen <gregoryg@vmware.com>
2025-12-07 21:13:50 -0500

    adding generator

---
+++ applications/generator-supplier-source/src/main/java/showcase/streaming/source/generator/AlertConfig.java
@@ -0,0 +1,17 @@
+package showcase.streaming.source.generator;
+
+import nyla.solutions.core.patterns.creational.generator.json.JsonGeneratorCreator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Properties;
+
+@Configuration
+public class AlertConfig {
+
+
+
+}
---
+++ applications/generator-supplier-source/src/main/java/showcase/streaming/source/generator/GeneratorSourceApp.java
@@ -0,0 +1,11 @@
+package showcase.streaming.source.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratorSourceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratorSourceApp.class,args);
+    }
+}
